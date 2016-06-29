9183e2af7bdcacf9187e330b71480069c0dcb002
anluong <anluong@microsoft.com>
2016-06-29 15:54:57 -0700

    Update tests

--- test/core/PowerBIToken.test.ts
+++ test/core/PowerBIToken.test.ts
@@ -5,7 +5,7 @@ import * as jwt from 'jwt-simple';
 let expect = chai.expect;
 
 describe('Power BI Token', () => {
-    const version: string = '0.1.0';
+    const version: string = '0.2.0';
     const issuer: string = 'Power BI Node SDK';
     const resource: string = 'https://analysis.windows.net/powerbi/api';
     const workspacCollection: string = 'TestCollection';
@@ -57,86 +57,6 @@ describe('Power BI Token', () => {
         });
     });
 
-    describe('Provision Tokens', () => {
-        it('can be created', () => {
-            let token = powerbi.PowerBIToken.createProvisionToken(workspacCollection);
-            let jwt = token.generate(accessKey);
-
-            expect(jwt).not.to.be.null;
-        });
-
-        it('are created with a default expiration', () => {
-            let nbf = powerbi.Util.getUnixTime(new Date());
-            let exp = nbf + 3600;
-
-            let devToken = powerbi.PowerBIToken.createProvisionToken(workspacCollection);
-            let token = devToken.generate(accessKey);
-            let decoded = jwt.decode(token, accessKey);
-
-            expect(decoded.nbf).to.equal(nbf);
-            expect(decoded.exp).to.equal(exp)
-        });
-
-        it('are created with the correct claims', () => {
-            let now = new Date();
-            let nbf = powerbi.Util.getUnixTime(now);
-            let exp = nbf + 3600;
-            let expiration = new Date(exp * 1000);
-
-            let devToken = powerbi.PowerBIToken.createProvisionToken(workspacCollection, expiration);
-            let token = devToken.generate(accessKey);
-            let decoded = jwt.decode(token, accessKey);
-
-            expect(decoded.ver).to.equal(version);
-            expect(decoded.aud).to.equal(resource);
-            expect(decoded.iss).to.equal(issuer);
-            expect(decoded.type).to.equal('provision');
-            expect(decoded.wcn).to.equal(workspacCollection);
-            expect(decoded.nbf).to.equal(nbf);
-            expect(decoded.exp).to.equal(exp);
-        });
-    });
-
-    describe('Dev Tokens', () => {
-        it('can be created', () => {
-            let token = powerbi.PowerBIToken.createDevToken(workspacCollection, workspaceId);
-            let jwt = token.generate(accessKey);
-
-            expect(jwt).not.to.be.null;
-        });
-
-        it('are created with a default expiration', () => {
-            let nbf = powerbi.Util.getUnixTime(new Date());
-            let exp = nbf + 3600;
-
-            let devToken = powerbi.PowerBIToken.createDevToken(workspacCollection, workspaceId);
-            let token = devToken.generate(accessKey);
-            let decoded = jwt.decode(token, accessKey);
-
-            expect(decoded.nbf).to.equal(nbf);
-            expect(decoded.exp).to.equal(exp)
-        });
-
-        it('are created with the correct claims', () => {
-            let nbf = powerbi.Util.getUnixTime(new Date());
-            let exp = nbf + 3600;
-            let expiration = new Date(exp * 1000);
-
-            let devToken = powerbi.PowerBIToken.createDevToken(workspacCollection, workspaceId, expiration);
-            let token = devToken.generate(accessKey);
-            let decoded = jwt.decode(token, accessKey);
-
-            expect(decoded.ver).to.equal(version);
-            expect(decoded.aud).to.equal(resource);
-            expect(decoded.iss).to.equal(issuer);
-            expect(decoded.type).to.equal('dev');
-            expect(decoded.wcn).to.equal(workspacCollection);
-            expect(decoded.wid).to.equal(workspaceId);
-            expect(decoded.nbf).to.equal(nbf);
-            expect(decoded.exp).to.equal(exp);
-        });
-    });
-
     describe('Embed Tokens', () => {
         it('can be created', () => {
             let token = powerbi.PowerBIToken.createReportEmbedToken(workspacCollection, workspaceId, reportId);
@@ -169,7 +89,6 @@ describe('Power BI Token', () => {
             expect(decoded.ver).to.equal(version);
             expect(decoded.aud).to.equal(resource);
             expect(decoded.iss).to.equal(issuer);
-            expect(decoded.type).to.equal('embed');
             expect(decoded.wcn).to.equal(workspacCollection);
             expect(decoded.wid).to.equal(workspaceId);
             expect(decoded.rid).to.equal(reportId);
