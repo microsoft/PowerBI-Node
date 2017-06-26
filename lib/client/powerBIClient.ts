ec9bb9c51240ccb3ae37eba4ddff2b066bcef603
Diego Plascencia <diego.plascencia@netlogistik.com>
2017-06-26 13:32:21 -0500

    added generated api v2

--- lib/client/powerBIClient.ts
+++ lib/client/powerBIClient.ts
@@ -1,10 +1,20 @@
 'use strict';
 
 import AutorestClient = require('../autorest/powerBIClient');
+import AutorestClientV2 = require('../autorest/V2/powerBIClient');
 import * as operations from './operations';
 import {ServiceClientCredentials, ServiceClientOptions} from 'ms-rest';
 
 export class PowerBIClient extends AutorestClient {
+    public imports:operations.Imports;
+    
+    constructor(credentials: ServiceClientCredentials, baseUri: string='https://api.powerbi.com', options?: ServiceClientOptions) {
+        super(credentials, baseUri, options);
+        this.imports = new operations.Imports(this);
+    }
+}
+
+export class PowerBIClientV2 extends AutorestClientV2 {
     public imports:operations.Imports;
     
     constructor(credentials: ServiceClientCredentials, baseUri: string='https://api.powerbi.com', options?: ServiceClientOptions) {
