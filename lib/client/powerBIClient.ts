d36dd5a271c731ebfca2be4683d865acfbdc7b43
Diego Plascencia <diego.plascencia@netlogistik.com>
2017-06-26 15:13:12 -0500

    exported class for v2 sdk

--- lib/client/powerBIClient.ts
+++ lib/client/powerBIClient.ts
@@ -3,22 +3,22 @@
 import AutorestClient = require('../autorest/powerBIClient');
 import AutorestClientV2 = require('../autorest/V2/powerBIClient');
 import * as operations from './operations';
-import {ServiceClientCredentials, ServiceClientOptions} from 'ms-rest';
+import * as operationsv2 from '../autorest/V2/operations'
+import { ServiceClientCredentials, ServiceClientOptions } from 'ms-rest';
 
 export class PowerBIClient extends AutorestClient {
-    public imports:operations.Imports;
-    
-    constructor(credentials: ServiceClientCredentials, baseUri: string='https://api.powerbi.com', options?: ServiceClientOptions) {
+    public imports: operations.Imports;
+
+    constructor(credentials: ServiceClientCredentials, baseUri: string = 'https://api.powerbi.com', options?: ServiceClientOptions) {
         super(credentials, baseUri, options);
         this.imports = new operations.Imports(this);
     }
 }
 
 export class PowerBIClientV2 extends AutorestClientV2 {
-    public imports:operations.Imports;
-    
-    constructor(credentials: ServiceClientCredentials, baseUri: string='https://api.powerbi.com', options?: ServiceClientOptions) {
+    public imports: operationsv2.Imports;
+
+    constructor(credentials: ServiceClientCredentials, baseUri: string = 'https://api.powerbi.com', options?: ServiceClientOptions) {
         super(credentials, baseUri, options);
-        this.imports = new operations.Imports(this);
     }
 }
